dec07fb44c96369496e8d8fa7f7be31df78df8d6
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-01-07 12:19:59 +0100

    add responsive nav to header

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // end of toggle nav-links
 
     // listener for resize window and in case toggle class to navLink
+    addCloseClassToNavLink = () => {
+        let windowWidth = window.innerWidth;
+        if (windowWidth >= 768) {
+            NavLinks.classList.remove('close')
+        } else {
+            NavLinks.classList.add('close')
+        }
+
+    }
+    addCloseClassToNavLink();
+
 
     changeWindowSize = () => {
         let windowWidth = window.innerWidth

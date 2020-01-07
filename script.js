b5d5d6e9872c885a5489b2c4d5e523a6f5a4fe22
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-01-07 12:08:47 +0100

    done header

---
+++ script.js
@@ -0,0 +1,52 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    // Nav-Links toggle on small screen
+    const OpenBtn = document.querySelector(".nav-button");
+    const CloseBtn = document.querySelector('.close-btn')
+    const NavLinks = document.querySelector('.nav-links');
+
+    openNavLink = () => {
+        NavLinks.classList.remove('close')
+        OpenBtn.classList.add('hide')
+
+    }
+    closeNavLink = () => {
+        NavLinks.classList.add('close')
+        OpenBtn.classList.remove('hide')
+    }
+
+
+    OpenBtn.addEventListener('click', openNavLink)
+    CloseBtn.addEventListener('click', closeNavLink)
+
+    // end of toggle nav-links
+
+    // listener for resize window and in case toggle class to navLink
+
+    changeWindowSize = () => {
+        let windowWidth = window.innerWidth
+        if (windowWidth >= 768) {
+            NavLinks.classList.remove('close')
+            OpenBtn.classList.add('hide')
+        } else {
+            NavLinks.classList.add('close')
+            OpenBtn.classList.remove('hide')
+        }
+
+
+    }
+
+    window.addEventListener('resize', changeWindowSize);
+
+
+
+
+
+
+
+
+
+
+
+
+});
